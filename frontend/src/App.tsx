8133db8652ae485fe15c15fbdcb38ee0934d385a
Karl-Alexander <99612323+KarlMeierMattern@users.noreply.github.com>
2025-08-22 08:15:59 +0200

    commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,29 +4,29 @@ import { Signup } from "./pages/signup";
 import { Login } from "./pages/login";
 import { Dashboard } from "./pages/dashboard";
 import { ProtectedRoute } from "@/components/protected-route";
-// import { AuthGuard } from "@/components/auth-guard";
+import { AuthGuard } from "@/components/auth-guard";
 import { Navigate } from "react-router-dom";
 import { AllEntries } from "./pages/all-entries";
 
 const App = () => {
   return (
-    // <AuthGuard>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      />
-      <Route path="*" element={<Navigate to="/" replace />} />
-      <Route path="/all-entries" element={<AllEntries />} />
-    </Routes>
-    // </AuthGuard>
+    <AuthGuard>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/all-entries" element={<AllEntries />} />
+      </Routes>
+    </AuthGuard>
   );
 };
 
