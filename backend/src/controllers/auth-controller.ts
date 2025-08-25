1bdaaae1420f0edb1230f5fde7345d1aad7a720a
Karl-Alexander <99612323+KarlMeierMattern@users.noreply.github.com>
2025-08-25 07:55:42 +0200

    commit

--- backend/src/controllers/auth-controller.ts
+++ backend/src/controllers/auth-controller.ts
@@ -53,8 +53,8 @@ export const signup = async (
     // set cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       domain:
         process.env.NODE_ENV === "development"
@@ -125,8 +125,8 @@ export const login = async (
     // set cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       domain:
         process.env.NODE_ENV === "development"
