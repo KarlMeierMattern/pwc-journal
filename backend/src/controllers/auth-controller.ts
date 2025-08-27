14e2485fa110b8d7e9be6e401ffe8f19024c0511
Karl-Alexander <99612323+KarlMeierMattern@users.noreply.github.com>
2025-08-27 15:20:40 +0200

    coommit

--- backend/src/controllers/auth-controller.ts
+++ backend/src/controllers/auth-controller.ts
@@ -53,10 +53,13 @@ export const signup = async (
     // set cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: true,
+      secure: process.env.NODE_ENV === "production",
       sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-      domain: ".167.235.142.148.sslip.io",
+      domain:
+        process.env.NODE_ENV === "production"
+          ? ".167.235.142.148.sslip.io"
+          : undefined,
     });
 
     // return user
@@ -122,10 +125,13 @@ export const login = async (
     // set cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: true,
+      secure: process.env.NODE_ENV === "production",
       sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-      domain: ".167.235.142.148.sslip.io",
+      domain:
+        process.env.NODE_ENV === "production"
+          ? ".167.235.142.148.sslip.io"
+          : undefined,
     });
 
     // return user
