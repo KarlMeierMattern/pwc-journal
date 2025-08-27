8b1f34b61bc15a754228558992db76cba38bec8d
Karl-Alexander <99612323+KarlMeierMattern@users.noreply.github.com>
2025-08-27 16:21:40 +0200

    commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,18 @@
 import { Routes, Route } from "react-router-dom";
-import { Home } from "./pages/home";
+// import { Home } from "./pages/home";
 import { Signup } from "./pages/signup";
 import { Login } from "./pages/login";
 import { Dashboard } from "./pages/dashboard";
 import { ProtectedRoute } from "@/components/protected-route";
 import { AuthGuard } from "@/components/auth-guard";
-// import { Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { AllEntries } from "./pages/all-entries";
 
 const App = () => {
   return (
     <AuthGuard>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route
