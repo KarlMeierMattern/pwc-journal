77979ed27525593e03cbb35fd275cafe64ef9149
Karl-Alexander <99612323+KarlMeierMattern@users.noreply.github.com>
2025-11-06 07:52:53 +0200

    commit

--- backend/src/controllers/auth-controller.ts
+++ backend/src/controllers/auth-controller.ts
@@ -46,8 +46,8 @@ export const signup = async (
     // set cookie
     res.cookie("token", token, {
       httpOnly: true, // Prevents JavaScript access to cookie (blocks XSS attacks)
-      secure: false, // Cookie sent over HTTP (rather than HTTPS - risk of man-in-the-middle attacks)
-      sameSite: "lax", // Blocks cross-site requests except navigation links (prevents CSRF attacks)
+      secure: process.env.NODE_ENV === "production", // Cookie sent over HTTPS
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // Blocks cross-site requests except navigation links (prevents CSRF attacks)
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       domain:
         process.env.NODE_ENV === "production"
@@ -115,8 +115,8 @@ export const login = async (
     // set cookie
     res.cookie("token", token, {
       httpOnly: true, // Prevents JavaScript access to cookie (blocks XSS attacks)
-      secure: false, // Cookie sent over HTTP (rather than HTTPS - risk of man-in-the-middle attacks)
-      sameSite: "lax", // Blocks cross-site requests except navigation links (prevents CSRF attacks)
+      secure: process.env.NODE_ENV === "production", // Cookie sent over HTTPS
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // Blocks cross-site requests except navigation links (prevents CSRF attacks)
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       domain:
         process.env.NODE_ENV === "production"
